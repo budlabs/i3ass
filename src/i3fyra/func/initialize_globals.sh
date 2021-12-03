cef6a15f59055c2cf85dfd02c4bf5f4b6e36292d
budRich <robstenklippa@gmail.com>
2021-12-04 00:41:47 +0100

    [ADD] orientation() now works

--- src/i3fyra/func/initialize_globals.sh
+++ src/i3fyra/func/initialize_globals.sh
@@ -13,7 +13,7 @@ initialize_globals() {
 
   _marks_json=$(i3-msg -t get_marks)
 
-  [[ $_action != layout && ! $_marks_json =~ i3fyra_ws ]] && {
+  [[ ! $_action =~ orintation|layout && ! $_marks_json =~ i3fyra_ws ]] && {
     # the i3fyra_ws mark/var is read by i3list
     # if it isn't present no info regarding i3fyra
     # will be in the output of i3list
@@ -26,11 +26,16 @@ initialize_globals() {
       I3FYRA_WS=\"${i3list[WAN]}\"
     }
 
-    (( _o[float] && i3list[AWF]==0 )) || {
-      i3var set i3fyra_ws "$I3FYRA_WS"
-      i3var set i34ORI "$I3FYRA_ORIENTATION"
-    }
+    (( _o[float] && i3list[AWF]==0 )) \
+      || i3var set i3fyra_ws "$I3FYRA_WS"
 
+  }
+
+  [[ $_marks_json =~ i34ORI ]] || {
+
+    ((_o[verbose])) && ERM INIT FYRA_ORIENTATION
+
+    i3var set i34ORI "${_o[orientation]:-$I3FYRA_ORIENTATION}"
   }
 
   # _qflag is option added to i3-msg (cleanup())
@@ -46,11 +51,11 @@ initialize_globals() {
   declare -i swh=$((sw/2))
   declare -i shh=$((sh/2))
 
-  if [[ ${i3list[ori]} = vertical ]]; then
+  if [[ ${i3list[ORI]} = vertical ]]; then
     _isvertical=1
     ori=(
 
-      [main]=AC [fam1]=AB [fam2]=CD
+      [main]=AC [fam1]=AB [fam2]=CD [fam3]=BD
 
       [charmain]=v        [charfam]=h
       [movemain]=down     [movefam]=right
@@ -63,7 +68,7 @@ initialize_globals() {
     _isvertical=0
     ori=(
 
-      [main]=AB [fam1]=AC [fam2]=BD
+      [main]=AB [fam1]=AC [fam2]=BD [fam3]=CD
 
       [charmain]=h        [charfam]=v
       [movemain]=right    [movefam]=down
