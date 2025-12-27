face7ad5aea6b857f0f65448c168510998fa84e1
budRich <robstenklippa@gmail.com>
2025-12-27 15:46:06 +0100

    [FYRA][FIX] monocle, handle floating -> monocled
    What we do is disable floating and move the window to a fyra container, then hide others.
    we dont toggle back floating when de-monocling, it gets complicated and might very well
    not be what the user want anyways.
    
    Fixes #225

--- src/i3fyra/func/monocle.sh
+++ src/i3fyra/func/monocle.sh
@@ -9,7 +9,32 @@ monocle_toggle() {
     return
   }
 
-  if ((${#i3list[LVI]} < 2)); then
+  # AWF == 1, active window is floating
+  if ((i3list[AWF]==1)); then
+
+    if [[ ${i3list[LVI]} =~ $I3FYRA_MAIN_CONTAINER ]]; then
+      target=$I3FYRA_MAIN_CONTAINER
+    elif [[ ${i3list[LVI]} ]]; then
+      target=${i3list[LVI]:0:1}
+    elif [[ ${i3list[LHI]} ]]; then
+      target=${i3list[LHI]:0:1}
+    else
+      target=$I3FYRA_MAIN_CONTAINER
+    fi
+
+    if [[ ${i3list[LEX]} =~ $target ]]; then
+      container_show "$target"
+      messy "[con_id=${i3list[AWC]}]" floating disable, \
+        move to mark "i34${target}"
+    else
+      # if $target container doesn't exist, create it
+      container_show "$target"
+    fi
+
+    to_hide="${i3list[LVI]/$target/}"
+    [[ $to_hide ]] && multi_hide "$to_hide"
+
+  elif ((${#i3list[LVI]} < 2)); then
     # use family memory if it exist
     last_seen="${i3list[F${ori[fam1]}]}${i3list[F${ori[fam2]}]}"
     [[ $last_seen ]] \
